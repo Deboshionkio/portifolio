1c1bad2f5a6d712a8d2be98600763ea241b953a0
João Carlos Vieira <91760912+Joaojhonsax@users.noreply.github.com>
2022-07-25 20:17:29 -0300

    Update trocaImagem.js

--- trocaImagem.js
+++ trocaImagem.js
@@ -15,7 +15,7 @@ function inverteEstudo(imagem){
 
 
 function trocaSax(imagem){
-    imagem.src="file:///C:/Users/55489/OneDrive%20-%20UNIVALI/Documentos/visualcode%20projetos%20portifolio/imagens/sax.jpeg";
+    imagem.src="https://raw.githubusercontent.com/Joaojhonsax/portifolio/main/imagens/sax.jpeg";
 }
 function trocaSoprador(imagem){
     imagem.src="file:///C:/Users/55489/OneDrive%20-%20UNIVALI/Documentos/visualcode%20projetos%20portifolio/imagens/soprador.jpg";
