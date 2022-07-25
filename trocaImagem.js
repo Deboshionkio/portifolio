37c7f7881b42a966699a5e0ff861d2a938701c90
João Carlos Vieira <91760912+Joaojhonsax@users.noreply.github.com>
2022-07-25 16:24:43 -0300

    adicionados aqruivos

---
+++ trocaImagem.js
@@ -0,0 +1,22 @@
+function mostraBeijaflor(imagem){
+    imagem.src="file:///C:/Users/55489/OneDrive%20-%20UNIVALI/Imagens/portifolio%20joaocarlos/projeto%20portifolio/beijaflor.jpeg";
+   }
+function mostraSecador(imagem){
+    imagem.src="file:///C:/Users/55489/OneDrive%20-%20UNIVALI/Imagens/portifolio%20joaocarlos/projeto%20portifolio/secador.jpg";
+   };
+
+
+function inverteEstudante(imagem){
+    imagem.src="file:///C:/Users/55489/OneDrive%20-%20UNIVALI/Imagens/portifolio%20joaocarlos/projeto%20portifolio/estudante.jpeg";
+}
+function inverteEstudo(imagem){
+    imagem.src="file:///C:/Users/55489/OneDrive%20-%20UNIVALI/Imagens/portifolio%20joaocarlos/projeto%20portifolio/estudo.jpg";
+};
+
+
+function trocaSax(imagem){
+    imagem.src="file:///C:/Users/55489/OneDrive%20-%20UNIVALI/Documentos/visualcode%20projetos%20portifolio/imagens/sax.jpeg";
+}
+function trocaSoprador(imagem){
+    imagem.src="file:///C:/Users/55489/OneDrive%20-%20UNIVALI/Documentos/visualcode%20projetos%20portifolio/imagens/soprador.jpg";
+}
